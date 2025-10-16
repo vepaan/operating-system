0fe88cfef45766dfd0e7877de06ff4f4417c749a
Vision Panta <visionpanta147@gmail.com>
2025-10-15 23:25:49 -0500

    setting up types to avoid compiler confusion

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,8 @@
+#include "types.h"
+
 void printf(const char* str)
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000; // Video memory address
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000; // Video memory address
     
     for (int i=0; str[i] != '\0'; ++i) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // so that we dont overwrite color bit
@@ -19,7 +21,7 @@ extern "C" void callConstructors()
   }
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World!");
 
