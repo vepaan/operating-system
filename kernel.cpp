f6f867334bad87a2c47ad394dbd2eba3e0dfe8d9
Vision Panta <visionpanta147@gmail.com>
2025-10-18 16:54:36 -0500

    adding global descriptor table

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,12 @@
 #include "types.h"
+#include "gdt.h"
 
 void printf(const char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000; // Video memory address
     
-    for (int i=0; str[i] != '\0'; ++i) {
+    for (int i=0; str[i] != '\0'; ++i) 
+    {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // so that we dont overwrite color bit
     }
 }
@@ -24,6 +26,8 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World!");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 }
