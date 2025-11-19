0ce1e1b444c73b52e1e078752d696c03cf8ef76a
Vision Panta <visionpanta147@gmail.com>
2025-11-19 00:21:13 -0600

    adding vga abstraction for the os and drawing simple things in kernel

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -5,6 +5,7 @@
 #include <drivers/driver.h>
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
+#include <drivers/vga.h>
 
 using namespace myos;
 using namespace myos::common;
@@ -99,11 +100,20 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
       PeripheralComponentInterconnectController PCIController;
       PCIController.SelectDrivers(&drvManager, &interrupts);
 
+      VideoGraphicsArray vga;
+
     printf("Activating all drivers, Stage 2...\n");
     drvManager.ActivateAll();
 
     printf("Activating Interrupts, Stage 3...\n");
     interrupts.Activate();
+
+    vga.SetMode(320, 200, 8);
+
+    // drawing smth simple (blue rectangle)
+    for (int32_t y=0; y<200; ++y)
+      for (int32_t x=0; x<320; ++x)
+        vga.PutPixel(x, y, 0x00, 0x00, 0xA8);
 
     while(1);
 }
